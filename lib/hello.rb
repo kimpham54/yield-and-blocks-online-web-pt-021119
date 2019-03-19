416d158cf499d79c68a109e17147e62bf279f58c
proactive-hashmap-4080 <kimpham54@gmail.com>
2019-03-19 01:21:30 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,13 +1,12 @@
-# lib/hello.rb
-
 def hello_t(array)
   i = 0
  
   while i < array.length
-    yield array[i]
+    yield(array[i])
     i = i + 1
-    puts "#{i} value"
   end
+ 
+  array
 end
 
 # call your method here!
